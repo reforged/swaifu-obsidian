63fb48df21e6afafe4f1daf538981e164d7058cb
Alfonsi Loïc <loicalfonsi24@gmail.com>
2023-01-17 12:48:20 +0100

    creation data temporaire

---
+++ src/utils/data.ts
@@ -0,0 +1,7 @@
+import { IEtiquette } from ".";
+
+export const etiquettes: IEtiquette[] = [
+    {id: '1', label: 'Lorem ipsum', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', color: 'red'},
+    {id: '2',label: 'Test 2', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', color: 'blue'},
+    {id: '3',label: 'Java', description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.', color: 'gray'},
+]
