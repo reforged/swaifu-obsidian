dc0d17ef9e27ace34bb29922bcb1048acec33b8e
NathaelB <kirua48000@gmail.com>
2023-01-17 09:59:24 +0100

    :construction: Create Route Group `etiquettes`

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import {Route, Routes} from 'react-router'
 import Home from './pages'
 import Layout from "./layouts/layout";
+import HomeEtiquette from "./pages/etiquettes";
 
 function App() {
   return (
     <Routes>
       <Route path={"/"} element={<Layout/>}>
         <Route index element={<Home />} />
+
+        <Route path={"/etiquettes"}>
+          <Route index element={<HomeEtiquette />} />
+        </Route>
       </Route>
     </Routes>
   )
