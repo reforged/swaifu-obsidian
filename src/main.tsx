ade80cbeb8a6fca76b7f0d338a8aea48a30d4ec3
NathaelB <kirua48000@gmail.com>
2023-01-19 11:52:57 +0100

    :lipstick: import styles

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import { QueryClient, QueryClientProvider } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 import { BrowserRouter } from 'react-router-dom'
+
+import './styles/tailwind.css'
+import './styles/highlight.scss'
+import './styles/global.scss'
+
 
 const queryClient = new QueryClient()
 
