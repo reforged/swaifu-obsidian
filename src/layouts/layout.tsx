a3d6af00326f2990c3081fe582c39184a5eed1fb
NathaelB <kirua48000@gmail.com>
2023-01-20 01:32:51 +0100

    :construction: Layout Provider

--- src/layouts/layout.tsx
+++ src/layouts/layout.tsx
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Outlet } from 'react-router'
 import Sidebar from '../components/Sidebar'
+import Manager from './manager'
 
 export default function Layout ()  {
   return (
-    <div className="hidden lg:flex lg:flex-shrink-0">
-      <Sidebar className="flex w-64 min-h-screen flex-col border-r border-gray-200" />
-      <div className="flex min-w-0 flex-1 flex-col overflow-hidden w-full">
+    <Manager>
+      <div className="hidden lg:flex lg:flex-shrink-0">
+        <Sidebar className="flex w-64 min-h-screen flex-col border-r border-gray-200" />
+        <div className="flex min-w-0 flex-1 flex-col overflow-hidden w-full">
 
-        <div className="px-5 max-w-7xl mx-auto w-full pt-12">
-          <Outlet />
+          <div className="px-5 max-w-7xl mx-auto w-full pt-12">
+            <Outlet />
+          </div>
         </div>
       </div>
-    </div>
-
+    </Manager>
   )
 }
