5afe01916d700f7bd4180bfb9daea5db8a1695ad
NathaelB <kirua48000@gmail.com>
2023-01-30 13:30:27 +0100

    :construction: update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,51 +1,116 @@
-import React from 'react'
-import { ChatBubbleLeftRightIcon, RocketLaunchIcon } from '@heroicons/react/24/outline'
-import { NavLink } from 'react-router-dom'
-import { AuthenticationContext } from '../contexts/AuthenticationContext'
+import React, {useState} from 'react'
+import DarkMode from "../components/DarkMode";
+import { ChevronRightIcon } from '@heroicons/react/20/solid'
+import Navbar from "../components/diamond/Navbar";
 
-export default function Home () {
+export default function Index () {
+  const [open, setOpen] = useState<boolean>(false)
   return (
-    <AuthenticationContext.Consumer>
-      {({user}) => (
-        <div>
-          <h1 className="text-2xl font-medium">Good morning, {user?.email} 👋</h1>
+    <div>
+      <div className="relative isolate overflow-hidden bg-white dark:bg-gray-900">
+        <Navbar open={open} setOpen={setOpen} />
+        <svg
+          className="absolute inset-0 -z-10 h-full w-full stroke-gray-200 dark:stroke-white/10 [mask-image:radial-gradient(100%_100%_at_top_right,white,transparent)]"
+          aria-hidden="true"
+        >
+          <defs>
+            <pattern
+              id="0787a7c5-978c-4f66-83c7-11c213f99cb7"
+              width={200}
+              height={200}
+              x="50%"
+              y={-1}
+              patternUnits="userSpaceOnUse"
+            >
+              <path d="M.5 200V.5H200" fill="none" />
+            </pattern>
+          </defs>
+          <rect width="100%" height="100%" strokeWidth={0} fill="url(#0787a7c5-978c-4f66-83c7-11c213f99cb7)" />
+        </svg>
+        <svg
+          viewBox="0 0 1108 632"
+          aria-hidden="true"
+          className="absolute top-10 left-[calc(50%-4rem)] -z-10 w-[69.25rem] max-w-none transform-gpu blur-3xl sm:left-[calc(50%-18rem)] lg:left-48 lg:top-[calc(50%-30rem)] xl:left-[calc(50%-24rem)]"
+        >
+          <path
+            fill="url(#175c433f-44f6-4d59-93f0-c5c51ad5566d)"
+            fillOpacity=".2"
+            d="M235.233 402.609 57.541 321.573.83 631.05l234.404-228.441 320.018 145.945c-65.036-115.261-134.286-322.756 109.01-230.655C968.382 433.026 1031 651.247 1092.23 459.36c48.98-153.51-34.51-321.107-82.37-385.717L810.952 324.222 648.261.088 235.233 402.609Z"
+          />
+          <defs>
+            <linearGradient
+              id="175c433f-44f6-4d59-93f0-c5c51ad5566d"
+              x1="1220.59"
+              x2="-85.053"
+              y1="432.766"
+              y2="638.714"
+              gradientUnits="userSpaceOnUse"
+            >
+              <stop stopColor="#4F46E5" />
+              <stop offset={1} stopColor="#80CAFF" />
+            </linearGradient>
+          </defs>
+        </svg>
+        <div className="mx-auto max-w-7xl px-6 pt-10 pb-24 sm:pb-32 lg:flex lg:py-40 lg:px-8">
+          <div className="mx-auto max-w-2xl lg:mx-0 lg:max-w-xl lg:flex-shrink-0 lg:pt-8">
+           <img
+                className="h-11 w-11 object-cover rounded-md"
+                src="https://cdn.discordapp.com/attachments/1052152529682710558/1064960787426324531/dzadazdazda.png"
+                alt="Your Company"
+              />
 
-          <div className="mt-5 p-5 bg-purple-100 rounded-md inline-flex w-full items-center justify-between">
-            <div className="flex gap-5">
-              <div className="bg-white w-10 h-10 flex items-center justify-center rounded-md">
-                <RocketLaunchIcon className="w-6 h-6 text-purple-800" />
-              </div>
-              <div>
-                <p className="text-md font-medium">Profites des dernières features</p>
-                <p className="text-sm">Voici le nouvel abonnement profites de nouveau éditeur et de la création de workspace!</p>
-              </div>
+            <div className="mt-24 sm:mt-32 lg:mt-16">
+              <a href="#" className="inline-flex space-x-6">
+              <span className="rounded-full bg-indigo-600/10 dark:bg-indigo-500/10 px-3 py-1 text-sm font-semibold leading-6 text-indigo-600 dark:text-indigo-400 ring-1 ring-inset ring-indigo-600/10 dark:ring-indigo-500/20">
+                What's new
+              </span>
+                <span className="inline-flex items-center space-x-2 text-sm font-medium leading-6 text-gray-600 dark:text-gray-400">
+                <span>Just shipped v1.0</span>
+                <ChevronRightIcon className="h-5 w-5 text-gray-400 dark:text-gray-500" aria-hidden="true" />
+              </span>
+              </a>
             </div>
-            <div>
-              <button className="px-3 py-1.5 inline-flex items-center justify-center bg-purple-700 hover:bg-purple-800 text-white font-medium rounded-md">Subscribe</button>
+            <h1 className="mt-10 text-4xl font-bold tracking-tight text-gray-900 dark:text-white sm:text-6xl">
+              Deploy to the cloud with confidence
+            </h1>
+            <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-500">
+              Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo. Elit sunt amet
+              fugiat veniam occaecat fugiat aliqua.
+            </p>
+            <div className="mt-10 flex items-center gap-x-6">
+              <a
+                href="#"
+                className="rounded-md bg-indigo-600 px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-indigo-500 dark:bg-indigo-500 dark:hover:bg-indigo-400 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Get started
+              </a>
+              <a href="#" className="text-base font-semibold leading-7 text-gray-900">
+                Learn more <span aria-hidden="true">→</span>
+              </a>
             </div>
           </div>
-          <div className="mt-10 py-5">
-            <div className="inline-flex w-full items-center justify-between">
-              <h2 className="text-lg font-medium">Les derniers questionnaires créés</h2>
-              <NavLink to="/questions" className="text-purple-800 text-purple-900 font-medium">See all</NavLink>
-            </div>
-            <div className="py-5 grid grid-cols-4 gap-5">
-              {[0, 1, 2, 3].map((n): JSX.Element => (
-                <div key={n} className="col-span-1 border border-gray-200 rounded-md shadow hover:shadow-xl p-5">
-                  <div className="w-10 h-10 flex items-center justify-center">
-                    <ChatBubbleLeftRightIcon className="w-5 h-5 text-gray-800" />
-                  </div>
-                  <div className="space-y-2">
-                    <p className="text-md font-medium">Lorem ipsum dolor sir amet</p>
-                    <p className="text-sm">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-                  </div>
-                </div>
-              ))}
+          <div className="mx-auto mt-16 flex max-w-2xl sm:mt-24 lg:ml-10 lg:mt-0 lg:mr-0 lg:max-w-none lg:flex-none xl:ml-32">
+            <div className="max-w-3xl flex-none sm:max-w-5xl lg:max-w-none">
+              <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+                  <img
+                    src="https://tailwindui.com/img/component-images/dark-project-app-screenshot.png"
+                    alt="App screenshot"
+                    width={2432}
+                    height={1442}
+                    className="w-[76rem] rounded-md bg-white/5 shadow-2xl ring-1 ring-white/10 hidden dark:block"
+                  />
+                  <img
+                    src="https://tailwindui.com/img/component-images/project-app-screenshot.png"
+                    alt="App screenshot"
+                    width={2432}
+                    height={1442}
+                    className="w-[76rem] rounded-md shadow-2xl ring-1 ring-gray-900/10 block dark:hidden"
+                  />
+              </div>
             </div>
           </div>
         </div>
-      )}
-
-    </AuthenticationContext.Consumer>
+      </div>
+    </div>
   )
 }
