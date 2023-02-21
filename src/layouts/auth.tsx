eb27f5684606759e2a492bdb8967be9096bf1420
NathaelB <kirua48000@gmail.com>
2023-02-21 21:15:16 +0100

    :construction: work for Profil Page

---
+++ src/layouts/auth.tsx
@@ -0,0 +1,22 @@
+import {useContext} from "react";
+import {AuthenticationContext} from "../contexts/AuthenticationContext";
+import {Outlet, useNavigate} from "react-router";
+import useMe from "../hooks/useMe";
+
+export default function Auth () {
+  const { user } = useContext(AuthenticationContext)
+  const router = useNavigate()
+
+  const { data } = useMe()
+
+  if (!user) {
+    router('/login')
+    return <div>Non Connecté</div>
+  }
+
+  return (
+    <div>
+      <Outlet />
+    </div>
+  )
+}
