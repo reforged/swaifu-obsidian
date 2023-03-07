dac2051a31b12068d2e3474dd76cf01cd8c38ec3
NathaelB <kirua48000@gmail.com>
2023-03-07 01:15:29 +0100

    :construction: start Data visualization template

--- src/pages/manager/qcm/sequences/index.tsx
+++ src/pages/manager/qcm/sequences/index.tsx
@@ -1,22 +1,38 @@
-import React, {useContext, useEffect, useState} from 'react'
-import {AuthenticationContext} from "../../../contexts/AuthenticationContext";
-import ShowQuestion from "../../../components/manager/pages/ShowQuestion";
-import useComponentVisible from "../../../hooks/useComponentVisible";
+import React, {Dispatch, Fragment, SetStateAction, useContext, useEffect, useState} from 'react'
+import useComponentVisible from "../../../../hooks/useComponentVisible";
 import {AnimatePresence, motion} from "framer-motion";
-import {ArrowDownRightIcon, MinusIcon, XMarkIcon} from "@heroicons/react/20/solid";
-import {IQuestion, IReponse} from "@obsidian/type";
-import {Prose} from "../../../components/manager/Prose";
-import MarkDownRender from "../../../components/manager/questions/editor/MarkDownRender";
-import {classNames} from "../../../utils/helper";
-import useQuestions from "../../../hooks/use-questions";
-import useSequences from "../../../hooks/use-sequences";
+import { Menu, Transition } from '@headlessui/react'
+import {
+  ChevronRightIcon,
+  HomeIcon,
+} from "@heroicons/react/20/solid";
+import {IQuestion, IReponse, ISequence} from "../../../../utils";
+import MarkDownRender from "../../../../components/manager/questions/editor/MarkDownRender";
+import {classNames} from "../../../../utils/helper";
+import useQuestions from "../../../../hooks/use-questions";
+import useSequences from "../../../../hooks/use-sequences";
 import {useQuery} from "react-query";
-import CreateSequence from "../../../components/manager/sequences/CreateSequence";
+import CreateSequence from "../../../../components/manager/sequences/CreateSequence";
+import Hero from "../../../../components/manager/Hero";
+import {INavigation} from "../../../../utils";
+import {Link} from "react-router-dom";
+import {
+  FolderIcon,
+  PlusIcon,
+  QueueListIcon,
+  SwatchIcon,
+  ViewColumnsIcon
+} from "@heroicons/react/24/outline";
+import Search from "../../../../components/Search";
+import DragIcon from "../../../../components/icons/DragIcon";
+import SequenceContext from "../../../../contexts/SequenceContext";
 
 
 type ReponsesPreviewProps = {
   data: IReponse[]
 }
+
+type View = 'galerie' | 'liste'
 
 const ReponsesPreview = ({ data }: ReponsesPreviewProps) => {
   return (
@@ -43,61 +59,207 @@ const ReponsesPreview = ({ data }: ReponsesPreviewProps) => {
   )
 }
 
+const navigation: INavigation[] = [
+  { label: 'Home', href: '/manager/qcm'},
+  { label: 'Questions', href: '/manager/qcm/questions'},
+  { label: 'Etiquettes', href: '/manager/qcm/etiquettes'},
+  { label: 'Séquences', href: '/manager/qcm/sequences'},
+]
+
 export default function HomeSequence () {
   const { ref, isVisible, toggle } = useComponentVisible()
+  const [sequence, setSequence] = useState<ISequence>({
+    label: '',
+    questions: []
+  })
   const [selected, setSelected] = useState<number[]>([])
   const [questions, setQuestions] = useState<IQuestion[]>([])
   const { fetchByUser } = useQuestions()
   const { getAll } = useSequences()
   const { data } = useQuery('sequences', getAll)
-
+  const [search, setSearch] = useState<string>('')
+  const [view, setView] = useState<View>('liste')
 
   return (
-    <div className="relative p-4 w-full z-0 h-screen">
-      <div className="flex justify-between items-center border-b">
-        <div>
-          <h1>Title</h1>
-        </div>
+    <div className="h-full">
+      <SequenceContext.Provider value={[ sequence, setSequence ]}>
+        <div className="relative min-h-full">
+          <Hero navigation={navigation} />
+          <div className="p-12">
+            <Breadcrumbs />
+            <div className="mt-12">
+              <h1 className="text-title">Séquences</h1>
 
-        <div>
-          <button onClick={toggle}>Créer la page</button>
+              <div className="w-full bg-[#F7F9FC] rounded-md border mt-8">
+                <div className="flex items-center py-3 px-8 justify-between border-b">
+                  <div>
+                    <button className="flex items-center gap-2 border rounded-md px-3 py-2">
+                      <span>
+                        <PlusIcon className="w-6 " />
+                      </span>
+                      <span>Séquence</span>
+                    </button>
+                  </div>
+                  <div className="flex items-center gap-4">
+                    <Search value={search} setValue={setSearch} />
 
-        </div>
-      </div>
-      <div>
-        <div className="grid grid-cols-4 gap-3 pt-4 relative z-none">
-          { data && <span>Existe</span>
-          }
-          <div>
-            <button
-              type="button"
-              onClick={toggle}
-              className="relative w-full h-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-0"
-            >
-              <span className="mt-2 block text-sm font-medium text-gray-900">Créer une séquence</span>
-            </button>
+                    <div className="relative">
+                      <Menu>
+                        <Menu.Button>
+                          <div className="flex items-center gap-2 border rounded-md px-3 py-2">
+                       <span>
+                          <SwatchIcon className="w-6" />
+                        </span>
+                            <span>Filter</span>
+                          </div>
+                        </Menu.Button>
+                        <Transition
+                          as={Fragment}
+                          enter="transition ease-out duration-100"
+                          enterFrom="transform opacity-0 scale-95"
+                          enterTo="transform opacity-100 scale-100"
+                          leave="transition ease-in duration-75"
+                          leaveFrom="transform opacity-100 scale-100"
+                          leaveTo="transform opacity-0 scale-95"
+                        >
+                          <Menu.Items className="absolute right-0 p-4 mt-2 w-56 origin-top-right divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 z-50 focus:outline-none">
+                            filter
+                          </Menu.Items>
+                        </Transition>
+                      </Menu>
+                    </div>
+
+                    <div className="relative">
+                      <ViewForm setView={setView} view={view} />
+                    </div>
+
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div>
+              <div className="grid grid-cols-4 gap-3 pt-4 relative z-none">
+                { data && <span>Existe</span>
+                }
+                <div>
+                  <button
+                    type="button"
+                    onClick={toggle}
+                    className="relative w-full h-full rounded-lg border-2 border-dashed border-gray-300 p-12 text-center hover:border-gray-400 focus:outline-none focus:ring-0"
+                  >
+                    <span className="mt-2 block text-sm font-medium text-gray-900">Créer une séquence</span>
+                  </button>
+                </div>
+              </div>
+            </div>
+
+            <div>
+              <AnimatePresence>
+                { isVisible &&
+                  <motion.div
+                    className=""
+                    animate={{opacity: 1}}
+                    transition={{
+                      duration: 0.2,
+                      ease: [0.5, 0.71, 1, 1.5],
+                    }}
+                    exit={{opacity: 0}}
+                    initial={{opacity: 0}}
+                  >
+                    <CreateSequence toggle={toggle} />
+                  </motion.div>
+                }
+              </AnimatePresence>
+            </div>
           </div>
         </div>
-      </div>
-
-      <div>
-        <AnimatePresence>
-          { isVisible &&
-            <motion.div
-              className=""
-              animate={{opacity: 1}}
-              transition={{
-                duration: 0.2,
-                ease: [0.5, 0.71, 1, 1.5],
-              }}
-              exit={{opacity: 0}}
-              initial={{opacity: 0}}
-            >
-              <CreateSequence toggle={toggle} />
-            </motion.div>
-          }
-        </AnimatePresence>
-      </div>
+      </SequenceContext.Provider>
     </div>
+
+
+  )
+}
+
+type ViewProps = {
+  view: View
+  setView: Dispatch<SetStateAction<View>>
+}
+function ViewForm ({ view, setView}: ViewProps) {
+  return (
+    <div>
+      <>
+        { view === 'galerie' && <Galerie view={view} setView={setView} />}
+        { view === 'liste' && <Liste view={view} setView={setView} /> }
+      </>
+
+    </div>
+  )
+}
+
+function Galerie ({ setView }: ViewProps) {
+  return (
+    <button
+      className="flex items-center gap-2 border rounded-md px-3 py-2"
+      onClick={() => {
+        setView('liste')
+      }}
+    >
+      <span>
+        <FolderIcon className="w-6" />
+      </span>
+      <span>Galerie</span>
+    </button>
+  )
+}
+
+function Liste ({ setView }: ViewProps) {
+  return (
+    <button
+      className="flex items-center gap-2 border rounded-md px-3 py-2"
+      onClick={() => {
+        setView('galerie')
+      }}
+    >
+      <span>
+        <QueueListIcon className="w-6" />
+      </span>
+      <span>Liste</span>
+    </button>
+  )
+}
+
+function Breadcrumbs () {
+  const pages = [
+    { name: 'QCM', href: '/manager/qcm', current: false},
+    { name: 'Séquences', href: '/manager/qcm/sequences', current: true},
+  ]
+
+  return (
+    <nav className="flex" aria-label="Breadcrumb">
+      <ol role="list" className="flex items-center space-x-4">
+        <li>
+          <div>
+            <Link to={'/manager/home'} className="text-gray-400 hover:text-gray-500">
+              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+              <span className="sr-only">Home</span>
+            </Link>
+          </div>
+        </li>
+        { pages.map((page) => (
+          <li key={page.name}>
+            <div className="flex items-center">
+              <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+              <Link
+                to={page.href}
+                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                aria-current={page.current ? 'page' : undefined}
+              >
+                {page.name}
+              </Link>
+            </div>
+          </li>
+        ))}
+      </ol>
+    </nav>
   )
 }
