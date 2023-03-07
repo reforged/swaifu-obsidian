8e22476803b0ba3d862dec51dad856d8adfcd84b
NathaelB <kirua48000@gmail.com>
2023-03-07 01:14:25 +0100

    :construction: rework UI

--- src/components/manager/Profil.tsx
+++ src/components/manager/Profil.tsx
@@ -4,6 +4,8 @@ import { AuthenticationContext } from '../../contexts/AuthenticationContext'
 import { IUser } from '../../utils'
 import { classNames } from '../../utils/helper'
 import userLogout from '../../hooks/user-logout'
+import {Link} from "react-router-dom";
+import {Cog6ToothIcon} from "@heroicons/react/24/outline";
 
 export default function Profil () {
   const { mutate: logout } = userLogout()
@@ -14,10 +16,17 @@ export default function Profil () {
         <>
           { user &&
             <Menu>
-              <Menu.Button>
-                <span className="px-2 py-2 border rounded-md bg-gray-50 shadow-sm">
-                  { user.email }
-                </span>
+              <Menu.Button className="mx-auto w-full">
+                <div className="flex items-center gap-3 hover:bg-[#E2E9F3] px-3 py-2 rounded-md">
+                  <div className="bg-white text-slate-600 font-medium h-12 w-12 justify-center rounded-full flex text-lg  focus:ring-offset-2 items-center inline-block align-center mx-auto focus:ring-indigo-500">
+                   <span className="">
+                      { user.firstname[0] }
+                    </span>
+                  </div>
+                  <span>{ user.firstname } { user.lastname }</span>
+                </div>
+
+
               </Menu.Button>
               <SubMenu user={user} logout={logout}/>
             </Menu>
@@ -44,27 +53,33 @@ const SubMenu = ({ user, logout }: MenuProps) => {
         leaveFrom="transform opacity-100 scale-100"
         leaveTo="transform opacity-0 scale-95"
       >
-        <div className="absolute rounded-md left-0 -top-44 mt-2 origin-top-left divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
-          <div className="px-4 py-3">
-            <p className="text-sm">Connecté avec</p>
-            <p className="truncate text-sm font-medium text-gray-900">{ user.email }</p>
-          </div>
+        <div className="absolute rounded-md right-0 top-16 mt-2 origin-top-left divide-y divide-gray-100 p-2 gap-3 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none z-50">
 
           <div className="py-1">
-            <a
-              href="src/components/manager/Profil#"
-              className={classNames(
-                'block px-4 py-2 text-sm'
-              )}
-            >
-              Account settings
-            </a>
+            <Link to={"/profil"}>
+              <div className="flex items-center justify-between">
+                <div className="flex items-center gap-2">
+                  <div className="bg-slate-100 text-slate-600 font-medium h-12 w-12 justify-center rounded-full flex text-lg  focus:ring-offset-2 items-center inline-block align-center mx-auto focus:ring-indigo-500">
+                     <span className="">
+                      { user.firstname[0] }
+                    </span>
+                  </div>
+                  <div className="flex flex-col">
+                    <span>{ user.firstname} {user.lastname}</span>
+                    <span>{ user.email}</span>
+                  </div>
+                <div>
+                  <Cog6ToothIcon className="w-6 h-6" />
+                </div>
+              </div>
+              </div>
+            </Link>
           </div>
 
           <div className="py-1">
             <button
               type="button"
-              onClick={() => logout()}
+              onClick={logout}
               className={classNames(
                 'block w-full px-4 py-2 text-left text-sm'
               )}
--- src/components/manager/Sidebar.tsx
+++ src/components/manager/Sidebar.tsx
@@ -1,8 +1,17 @@
 import React, {Fragment, useState} from 'react'
 import { NavLink, Link } from 'react-router-dom'
-import {HomeIcon, TagIcon, ChatBubbleBottomCenterTextIcon, UserIcon, DocumentTextIcon} from '@heroicons/react/24/solid'
+import {
+  HomeIcon,
+  ListBulletIcon,
+  ChatBubbleBottomCenterTextIcon,
+  UserIcon,
+  DocumentTextIcon,
+  UserGroupIcon
+} from '@heroicons/react/24/solid'
 import { classNames, ReactElement } from '../../utils/helper'
 import Profil from "./Profil";
+import {Menu, Transition} from "@headlessui/react";
+import {InformationCircleIcon} from "@heroicons/react/24/outline";
 
 type LinkItem = {
   label: string
@@ -13,13 +22,15 @@ type LinkItem = {
 
 export default function Sidebar ({ ...props }) {
   const links: LinkItem[] = [
-    { label: 'Home', href: '/manager', icon: HomeIcon },
-    {
+    { label: 'Home', href: '/manager/home', icon: HomeIcon },
+    { label: 'QCM', href: '/manager/qcm', icon: ListBulletIcon},
+    { label: 'Comptes', href: '/manager/comptes', icon: UserGroupIcon}
+    /*{
       label: 'QCM',
       children: [
         { label: 'Étiquettes', href: '/manager/etiquettes', icon: TagIcon},
         { label: 'Questions', href: '/manager/questions', icon: ChatBubbleBottomCenterTextIcon},
-        { label: 'Pages', href: '/manager/pages', icon: DocumentTextIcon}
+        { label: 'Séquences', href: '/manager/sequences', icon: DocumentTextIcon}
       ]
     },
     {
@@ -27,46 +38,93 @@ export default function Sidebar ({ ...props }) {
       children: [
         { label: 'Utilisateurs', href: '/manager/users', icon: UserIcon},
       ]
-    }
+    }*/
   ]
 
   const [open, setOpen] = useState<boolean>(false)
 
   return (
     <div {...props}>
-      <div className="px-5 py-5 flex flex-col justify-between h-full">
-        <div>
-          <Link to={"/"} className="inline-flex items-center space-x-2">
-            <div className="w-10 h-10 flex items-center justify-center bg-purple-600 rounded-md">
-              <img className="h-full w-full object-cover rounded-md" src="https://cdn.discordapp.com/attachments/1052152529682710558/1064960787426324531/dzadazdazda.png" alt=""/>
+      <div className="flex flex-col justify-between h-full bg-white">
+          <Link to={"/"} className="inline-flex border">
+            <div className="absolute inset-y-0 left-0 md:static md:flex-shrink-0">
+              <a
+                href="#"
+                className="flex h-20 w-20 items-center justify-center  focus:outline-none "
+              >
+                <img
+                  className="h-12 w-auto"
+                  src="https://cdn.discordapp.com/attachments/935833137349541918/1071833266841210961/branding.png"
+                  alt="Your Company"
+                />
+              </a>
             </div>
-            <p className="font-medium tracking-wide">ONU</p>
           </Link>
-          <div className="pt-12">
-            {links.map((link) => link.children
-              ? <ChildLinks key={link.label} link={link} />
-              : <LinkItem key={link.label} link={link} /> )}
+          <div className="flex flex-col justify-between border-r h-full py-8">
+            <div className=" flex flex-col gap-4">
+              {links.map((link) =>
+                <LinkItem key={link.label} link={link} /> )
+              }
+            </div>
+
+            <div>
+              <div className="relative flex flex-col gap-4 py-4">
+                <Info />
+              </div>
+            </div>
           </div>
-        </div>
-        <div>
-          <div className="relative">
-            <Profil />
-          </div>
+
         </div>
 
-      </div>
+
+    </div>
+  )
+}
+
+function Info () {
+  return (
+    <div className="mx-auto relative">
+      <Menu>
+        <Menu.Button >
+          {(active) => (
+            <div className={classNames(
+              active.open ? 'bg-[#E2E9F3] text-[#5B50D6]' : '',
+              'flex rounded-md p-3 mx-auto hover:bg-[#E2E9F3] hover:text-[#5B50D6] ease-in-out duration-200'
+            )}>
+              <InformationCircleIcon className="w-6 h-auto" />
+            </div>
+
+          )}
+
+        </Menu.Button>
+        <Transition>
+          <div className="absolute rounded-md left-[100%] ml-4 bottom-0 w-72 origin-top-left rounded-md bg-white shadow-lg flex flex-1">
+            <div className="flex flex-col flex-1 p-4">
+              <span className="text-gray-400">Besoin d'aide ?</span>
+              <div className="flex flex-col flex-1 pt-4">
+                <a href="https://docs.reforged.fr" target="_blank">
+                  <div className="flex items-center gap-2 p-2 group hover:bg-[#E2E9F3] rounded-md">
+                     <span>
+                      <DocumentTextIcon className="w-6 h-6 text-[#7A71DE]" />
+                    </span>
+                    <span className="text-gray-700 font-medium group-hover:text-[#7A71DE]">Voir la documentation</span>
+                  </div>
+
+                </a>
+              </div>
+            </div>
+          </div>
+        </Transition>
+      </Menu>
     </div>
   )
 }
 
 function LinkItem ({ link }: { link: LinkItem }): JSX.Element {
   return (
-    <NavLink to={link.href!} className={({ isActive }) => classNames('px-3 py-2 rounded-md flex space-x-2', isActive ? 'bg-purple-100' : '')}>
+    <NavLink to={link.href!} className={({ isActive }) => classNames('flex rounded-md p-3 mx-auto hover:bg-[#E2E9F3] hover:text-[#5B50D6] ease-in-out duration-200', isActive ? 'bg-[#E2E9F3]' : '')}>
       {({ isActive }) => (
-        <Fragment>
-          <ReactElement tag={link.icon!} className={classNames('w-5', isActive ? 'text-purple-600' : '')} />
-          <span className={classNames('font-medium text-sm', isActive ? 'text-purple-600' : '')}>{link.label}</span>
-        </Fragment>
+          <ReactElement tag={link.icon!} className={classNames('w-6', isActive ? 'text-[#5B50D6]' : '')} />
       )}
     </NavLink>
   )
