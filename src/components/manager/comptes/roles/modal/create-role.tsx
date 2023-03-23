2023109f2c2f68bd644fadd84d67347b97b0c94a
NathaelB <kirua48000@gmail.com>
2023-03-23 18:51:24 +0100

    :construction: work in UI and ts

--- src/components/manager/comptes/roles/modal/create-role.tsx
+++ src/components/manager/comptes/roles/modal/create-role.tsx
@@ -1,16 +1,34 @@
 import useComponentVisible from "../../../../../hooks/useComponentVisible";
 import {AnimatePresence, motion} from "framer-motion";
-import {useContext, useEffect} from "react";
+import React, {useContext, useEffect, useState} from "react";
 import BoardContext from "../../../../../contexts/BoardContext";
 import {XMarkIcon} from "@heroicons/react/20/solid";
 import {UserPlusIcon} from "@heroicons/react/24/outline";
+import useRoles from "../../../../../hooks/use-roles";
+import {classNames} from "../../../../../utils/helper";
 
 
 export default function CreateRole () {
   const { ref, isVisible, toggle, setIsVisible } = useComponentVisible()
-
   const [board, setBoard] = useContext(BoardContext)
 
+  const [label, setLabel] = useState<string>('')
+  const [power, setPower] = useState(0)
+  const [disabled, setDisabled] = useState<boolean>(true)
+  const { index } = useRoles()
+  const { data: roles} = index()
+
+  useEffect(() => {
+    if (label && power && roles) {
+      console.log(roles)
+      for (let i = 0; i < roles.length; i++) {
+        if (roles[i].label === label) setDisabled(true)
+        else setDisabled(false)
+      }
+    } else {
+      setDisabled(true)
+    }
+  }, [label, power])
 
   useEffect(() => {
     if (board.open) setIsVisible(true)
@@ -52,17 +70,55 @@ export default function CreateRole () {
                     <h1 className="font-title  font-medium text-2xl text-gray-700 text-center">Roles</h1>
                   </div>
                   <div className=" h-full">
-                      <div className="grid grid-cols-12 divide-x h-full">
+                      <div className="">
                         <div className="col-span-4">
                             <div className="flex flex-col text-left p-8 gap-2">
-                                <div className="flex gap-3 items-center">
-                                  <span>
-                                    <UserPlusIcon className="w-6" />
-                                  </span>
-                                  <span className="text-left">Créer un Roles</span>
+                              <div className="">
+                                <div className="grid grid-cols-6 gap-6">
+                                  <div className="col-span-6 sm:col-span-3">
+                                    <label htmlFor="first-name" className="block text-sm font-medium leading-6 text-gray-900">
+                                      Label
+                                    </label>
+                                    <input
+                                      type="text"
+                                      name="first-name"
+                                      id="first-name"
+                                      onChange={(e) => setLabel(e.currentTarget.value)}
+                                      autoComplete="given-name"
+                                      className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                  </div>
+
+
+                                  <div className="col-span-6">
+                                    <label htmlFor="power" className="block text-sm font-medium leading-6 text-gray-900">
+                                      Power
+                                    </label>
+                                    <input
+                                      type="number"
+                                      name="power"
+                                      id="power"
+                                      pattern="[0-9]"
+                                      onChange={(e) => setPower(e.currentTarget.value)}
+                                      className="mt-2 block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                                    />
+                                  </div>
                                 </div>
+                              </div>
                             </div>
                         </div>
+                      </div>
+
+                      <div>
+                        <button
+                          disabled={disabled}
+                          className={classNames(
+                            'rounded-md px-3 py-2 border w-full',
+                            disabled ? 'text-gray-400 bg-gray-50' : 'bg-indigo-500 text-white'
+                          )}
+                        >
+                          Créer votre compte
+                        </button>
                       </div>
                   </div>
                 </div>
