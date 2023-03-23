c0011036bb7cebfca00d19f49f16fe0bdd1b983a
Red-Fang <97971973+Red-Fang@users.noreply.github.com>
2023-03-23 16:51:35 +0100

    delete-unuse-import

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,4 +1,4 @@
-import React, {Dispatch, SetStateAction, useState} from 'react'
+import React, {Dispatch, SetStateAction} from 'react'
 import {MagnifyingGlassIcon} from "@heroicons/react/20/solid";
 
 type Props = {
