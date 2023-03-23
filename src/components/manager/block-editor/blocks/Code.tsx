62497bf401ecf70e8d72886fc5f2d98f9735d811
NathaelB <kirua48000@gmail.com>
2023-03-23 23:34:03 +0100

    :construction: work for mermaid modal

--- src/components/manager/block-editor/blocks/Code.tsx
+++ src/components/manager/block-editor/blocks/Code.tsx
@@ -5,6 +5,10 @@ import SettingsContext from '../contexts/SettingsContext'
 import { useBlock } from '../utils'
 import ContentEditable from '../components/ContentEditable'
 import { ClipboardDocumentListIcon } from '@heroicons/react/24/outline'
+import {useState} from "react";
+import {Mermaid} from "../../Mermaid";
+import useComponentVisible from "../../../../hooks/useComponentVisible";
+import {AnimatePresence, motion} from "framer-motion";
 
 type Props = {
   uid: string
@@ -16,6 +20,8 @@ type Props = {
 }
 
 export default function Code (props: Props): JSX.Element {
+  const { ref, isVisible, toggle, setIsVisible } = useComponentVisible()
+  const [type, setType] = useState<'code' | 'mermaid'>('code')
   const { updateBlock } = useBlock(props.uid)
 
   function handleChange (field: string, value: string) {
@@ -24,6 +30,19 @@ export default function Code (props: Props): JSX.Element {
 
   async function handleClipboardCopy (): Promise<void> {
     await navigator.clipboard.writeText(props.fields.code)
+  }
+
+  function switchMode () {
+    if (type === 'code') {
+      setType('mermaid')
+    } else {
+      setType('code')
+    }
+  }
+
+  function showMermaidForm () {
+    console.log("DROP MENU")
+    toggle()
   }
 
   return (
@@ -40,26 +59,63 @@ export default function Code (props: Props): JSX.Element {
                   className="text-gray-500 text-opacity-50 text-xs"
                 />
               )}
-              <div>
+              <div className="flex items-center gap-2">
+                <div>
+                  <button onClick={switchMode}>
+                    { type === 'code' ?
+                      <span>Code</span>
+                      : <span>Mermaid</span>
+                    }
+                  </button>
+                </div>
                 <button onClick={handleClipboardCopy} className="w-8 h-8 flex items-center justify-center">
-                  <ClipboardDocumentListIcon className="w-5 h-5 text-white opacity-50 hover:opacity-100" />
+                  <ClipboardDocumentListIcon className="w-5 h-5 text-gray-600 opacity-50 hover:opacity-100" />
                 </button>
               </div>
             </div>
+            { type === 'code'
+             ? <CodeMirror
+                editable={settings.mode === 'editor'}
+                readOnly={settings.mode === 'preview'}
+                basicSetup={{
+                  tabSize: 2,
+                  lineNumbers: props.fields.lineNumbers,
+                }}
+                value={props.fields.code}
+                height="auto"
+                theme={githubLight}
+                extensions={[javascript({ jsx: true, typescript: true })]}
+                onChange={(value) => handleChange('code', value)}
+              />
 
-            <CodeMirror
-              editable={settings.mode === 'editor'}
-              readOnly={settings.mode === 'preview'}
-              basicSetup={{
-                tabSize: 2,
-                lineNumbers: props.fields.lineNumbers,
-              }}
-              value={props.fields.code}
-              height="auto"
-              theme={githubLight}
-              extensions={[javascript({ jsx: true, typescript: true })]}
-              onChange={(value) => handleChange('code', value)}
-            />
+              :
+              <button
+                onClick={showMermaidForm}
+                className={"relative"}
+              >
+                <Mermaid text={props.fields.code} />
+                <AnimatePresence>
+                  {isVisible &&
+                    <motion.div
+                      className="relative z-[100]"
+                      animate={{opacity: 1}}
+                      transition={{
+                        duration: 0.2,
+                        ease: [0.5, 0.71, 1, 1.5],
+                      }}
+                      exit={{opacity: 0}}
+                      initial={{opacity: 0}}
+                    >
+                      <div className={"bg-red-400 absolute top-0 left-0 z-[100]"} ref={ref}>
+                        lol
+                      </div>
+                    </motion.div>
+                  }
+                </AnimatePresence>
+              </button>
+
+            }
+
           </div>
         </div>
       )}
