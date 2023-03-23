863c88ab2afaadf2ad2008d280b674c6bd2bb1dd
NathaelB <kirua48000@gmail.com>
2023-03-23 15:44:35 +0100

    :construction: create wrapper

--- src/components/manager/board/logic/logic-wrapper.ts
+++ src/components/manager/board/logic/logic-wrapper.ts
@@ -1,30 +1,63 @@
-import {ConditionContract, CoupleConditionContract} from "../filter/types";
-import {isGroup} from "../filter/utils";
-import {IQuestion} from "../../../../utils";
-import Select from "./select";
+import { ConditionContract, ConditionGroupContract, CoupleConditionContract } from '../filter/types'
+import { isGroup } from '../filter/utils'
+import TextLogic from './text'
+import SelectLogic from './select'
 
 export default class LogicWrapper<T> {
-  private conditions: CoupleConditionContract[]
+  private readonly groupCondition: ConditionGroupContract
   private data: T[]
 
-  constructor(conditions: CoupleConditionContract[], data: T[]) {
-    this.conditions = conditions
+  constructor(conditions: ConditionGroupContract, data: T[]) {
+    this.groupCondition = conditions
     this.data = data
   }
 
-  public filteredData (): T[] {
-    return this.data
+  public filteredData () {
+    return this.data.filter((item) => this.verifWithConjunction(item))
   }
 
-  public resultConditionOfRow (data) {
-    const result = this.conditions.map((value) => {
-      if (!isGroup(value)) {
-        const condition = value as ConditionContract
-        if (typeof condition === 'string') {
+  public verifWithConjunction (data: T) {
+    const li = this.groupCondition.conditions.map((item) => {
+      if (isGroup(item)) {
+        return this.verifGroupConjunction((item as ConditionGroupContract), data)
+      } else {
+        return this.verifCondition((item as ConditionContract), data)
+      }
+    })
 
-        } else {
-          const objet = new Select(condition, )
-        }
+    if (this.groupCondition.conjunction === 'and') {
+      return !li.includes(false);
+    } else {
+      return !!li.includes(true);
+    }
+  }
+
+  public verifGroupConjunction (group: ConditionGroupContract, data: T) {
+    const li: boolean[] = this.verifRow(data, group.conditions)
+    if (group.conjunction === 'and') {
+      return !li.includes(false);
+    } else {
+      return !!li.includes(true);
+    }
+  }
+
+  public verifCondition (condition: ConditionContract, data: T): boolean {
+    if (typeof condition.value === 'string') {
+      const objet = new TextLogic(condition, data, condition.value)
+      return objet.evaluate()
+    } else {
+      const objet = new SelectLogic(condition, data, condition.value)
+      return objet.evaluate()
+    }
+  }
+
+  public verifRow (data: T, conditions: CoupleConditionContract[]): any {
+    return conditions.map((item) => {
+      if (isGroup(item)) {
+        const group = item as ConditionGroupContract
+        return this.verifRow(data, group.conditions)
+      } else {
+        return this.verifCondition(item as ConditionContract, data)
       }
     })
   }
