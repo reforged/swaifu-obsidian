20c38686e013f5f12ab5dd24080feea52217f557
Nathael Bonnal <kirua48000@gmail.com>
2023-04-02 11:05:24 +0200

    :construction: update websocket hook

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -1,7 +1,15 @@
+import {io} from "socket.io-client";
+
 const baseUrl = 'ws://localhost:3333'
 
 export function useWebsocket () {
+  const socket = io(baseUrl, {
+    autoConnect: true,
+    reconnectionAttempts: 15
+  })
 
+
+  return { socket }
 }
 
 export default useWebsocket
