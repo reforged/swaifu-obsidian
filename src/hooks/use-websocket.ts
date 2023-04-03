b44c0d287e5ee4d4679826f341ce846cc184ba36
Nathael Bonnal <kirua48000@gmail.com>
2023-04-03 09:06:16 +0200

    :construction:  update baseUrl

--- src/hooks/use-websocket.ts
+++ src/hooks/use-websocket.ts
@@ -1,6 +1,6 @@
 import {io} from "socket.io-client";
 
-const baseUrl = 'ws://localhost:3333'
+const baseUrl: string = 'wss://api.reforged.fr'
 
 export function useWebsocket () {
   const socket = io(baseUrl, {
