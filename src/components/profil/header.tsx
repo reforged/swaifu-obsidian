5d45e1aef419d2e17274969bf4caa6bf293c38c3
Nathael Bonnal <kirua48000@gmail.com>
2023-04-04 18:50:05 +0200

    :construction: create composant for personal information header

---
+++ src/components/profil/header.tsx
@@ -0,0 +1,66 @@
+import Avatar from './avatar'
+import AuthenticationContext from '../../contexts/AuthenticationContext'
+import {useContext, useEffect, useState} from "react";
+import {IRole} from "../../utils";
+
+export default function HeaderProfil () {
+  return (
+    <div className="flex items-center gap-12 w-full">
+      <Avatar />
+      <Information />
+    </div>
+  )
+}
+
+function Information () {
+  const [user, setUser] = useContext(AuthenticationContext)
+  const [role, setRole] = useState<IRole>()
+
+  useEffect(() => {
+    if (user) {
+      const data = user.roles.reduce((prev, current) => {
+        if (current.power > prev.power) {
+          return current
+        } else if (current.power === prev.power) {
+          return current.label < prev.label ? current : prev;
+        } else {
+          return prev;
+        }
+      })
+
+      if (data) {
+        setRole(data)
+      }
+    }
+
+  }, [user])
+  return (
+    <AuthenticationContext.Consumer>
+      {([ user ]) => (
+        <>
+          {user &&
+            <div className="flex flex-col gap-4 truncate w-2/3">
+              {role &&
+                <div>
+                 <span className="inline-flex items-center rounded-md bg-green-100 px-2.5 py-0.5 text-sm font-medium text-green-800">
+                  { role.label }
+                </span>
+                </div>
+              }
+              <div>
+                <span className="text-6xl text-gray-900 font-medium">
+                  { user.lastname } { user.firstname }
+                </span>
+              </div>
+              <div className="flex flex-col gap-2">
+                { user.email && <span className="text-gray-300">Adresse email <span className="text-gray-800 ml-4">{ user.email}</span></span> }
+                { user.numero && <span className="text-gray-300">Numéro étudiant <span className="text-gray-800 ml-4">{ user.numero}</span></span> }
+              </div>
+            </div>
+          }
+        </>
+      )}
+    </AuthenticationContext.Consumer>
+
+  )
+}
